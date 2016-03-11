7d11628b03449e3cd30dbb71097d077fb2987317
Paul Whitaker <paulwhit@live.com>
2016-03-10 22:46:36 -0500

    Added files via upload

---
+++ betabrite_timeandtemp.py
@@ -0,0 +1,50 @@
+import time
+import datetime
+import alphasign
+import pyowm
+
+def main():
+        sign = alphasign.Serial()
+        sign.connect()
+        sign.clear_memory()
+
+        # Set the time
+
+        timeString = alphasign.String(size=100, label="1")
+        tempString = alphasign.String(size=100, label="2")
+        humiString = alphasign.String(size=100, label="3")
+
+        helloText = alphasign.Text("%s%s %s%sF %s%s%%" %(alphasign.colors.DIM_RED, timeString.call(), 
+                alphasign.colors.YELLOW, tempString.call(), 
+                alphasign.colors.ORANGE, humiString.call()), label="A", mode=alphasign.modes.HOLD)
+        sign.allocate((timeString,tempString,humiString,helloText))
+        sign.set_run_sequence((helloText,))
+        sign.write(timeString)
+        sign.write(tempString)
+        sign.write(humiString)
+        sign.write(helloText)
+
+        while True:
+
+                owm = pyowm.OWM('163f58185b28688d805dae724b718025')
+                observation = owm.weather_at_place('Rockville,MD')
+                w = observation.get_weather()
+                currTemp = int(w.get_temperature('fahrenheit').get('temp'))
+                humidity = w.get_humidity()
+
+                currentTime = datetime.datetime.strftime(datetime.datetime.now(), "%H:%M")
+                stringData = currentTime
+# "%s%s %s%sF %s%s%%" %(alphasign.colors.DIM_RED, currentTime, alphasign.colors.YELLOW, currTemp, alphasign.colors.ORANGE, humidity)
+
+                timeString.data = currentTime
+                tempString.data = currTemp
+                humiString.data = humidity
+
+                sign.write(timeString)
+                sign.write(tempString)
+                sign.write(humiString)
+
+                time.sleep(10)
+
+if __name__ == "__main__":
+        main()
