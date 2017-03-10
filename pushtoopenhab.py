c4144230cc80a371faedcf845bcc1c9c5f0eeda0
Paul Whitaker <paulwhit@live.com>
2017-03-10 13:09:26 -0500

    Update pushtoopenhab.py

--- pushtoopenhab.py
+++ pushtoopenhab.py
@@ -11,6 +11,7 @@ import arrow
 import os
 import subprocess
 import string
+import paho.mqtt.publish as publish
 
 DEVNULL = open(os.devnull, 'wb')
 
@@ -26,112 +27,54 @@ def log_traceback(ex, ex_traceback=None):
         except:
             syslog.syslog("Unhandled exception with sending exception to syslog")
 
-def connectSerial():
-    try:
-        ser1 = serial.Serial('/dev/ttyACM1',115200,timeout=1)
-        return ser1
-    except Exception as ex:
-        syslog.syslog("Could not connect on /dev/ttyACM0")
-        _, _, ex_traceback = sys.exc_info()
-        log_traceback(ex, ex_traceback)
-        try:
-            ser1 = serial.Serial('/dev/ttyACM1',115200,timeout=1)
-            return ser1
-        except Exception as ex:
-            syslog.syslog("Could not connect on /dev/ttyACM1")
-            _, _, ex_traceback = sys.exc_info()
-            log_traceback(ex, ex_traceback)
+
+lastReadTime = arrow.utcnow()
 
 while 1:
-    try:
 
-        # Wait 30 seconds on startup
-        #time.sleep(30)
+    line = sys.stdin.readline()
 
-        ser = connectSerial()
-
-        lastReadTime = arrow.utcnow()
-
-        while 1:
-        
-            try:
-                currentTime = arrow.utcnow()
-
-                # If no response in last 30 seconds, retry the serial connection
-                if (currentTime > (lastReadTime + datetime.timedelta(seconds=60))):
-                    syslog.syslog("Nothing coming through on serial, retrying connection")
-
-                    try:
-                        ser.close()
-                    except:
-                        syslog.syslog("Could not close old connection")
-
-                    # Reset USB device
-                    p = subprocess.call(['/home/pi/usbreset','/dev/bus/usb/001/007'], stdout=DEVNULL, stderr=DEVNULL)
-                    ser = connectSerial()
-
-                    lastReadTime = arrow.utcnow()
-
-            except Exception as ex:
-                _, _, ex_traceback = sys.exc_info()
-                log_traceback(ex, ex_traceback)
-                time.sleep(1)
-                continue
-            #print "trying to read..."
-
-            response = ser.readline()
-
-            if len(response) > 0:
-                lastReadTime = arrow.utcnow()
-                nowFormatted = time.strftime('%Y-%m-%d %H:%M:%S')
-
-                try:
-                    channel,temperature,humidity = response.split(",")
-                except Exception as ex:
-                    _, _, ex_traceback = sys.exc_info()
-                    log_traceback(ex, ex_traceback)
-                    time.sleep(1)
-                    continue
-
-                for whitespace_character in string.whitespace:
-                    channel = channel.replace(whitespace_character,'')
-
-                for whitespace_character in string.whitespace:
-                    temperature = temperature.replace(whitespace_character,'')
-
-                for whitespace_character in string.whitespace:
-                    humidity = humidity.replace(whitespace_character,'')
-
-                #print channel
-                #print temperature
-                #print humidity
-               command = 'mosquitto_pub -h amelia -p 1883 -t home/temperature' + channel + ' -m \"' + str(temperature) + '\" -d'
-                #print 'Running command: ' + command
-                os.system(command)
-
-                command = 'mosquitto_pub -h amelia -p 1883 -t home/humidity' + channel + ' -m \"' + str(humidity) + '\" -d'
-                #print 'Running command: ' + command
-                os.system(command)
-
-                command = 'mosquitto_pub -h amelia -p 1883 -t home/lastupdate' + channel + ' -m \"' + str(datetime.datetime.now()) + '\" -d'
-                #print 'Running command: ' + command
-
-                os.system(command)
-
-                if (len(channel) > 0 and len(temperature) > 0 and len(humidity) > 0 and int(humidity) > 20 and float(temperature) > 60.0 and float(temperature) < 100.0):
-                    syslog.syslog(response)
-
-                    # Wait 10 seconds then retry
-                    time.sleep(10)
-
-    except Exception as ex:
+    if line.startswith('20'):
         try:
+            parts = line.split(',')
+            date = parts[0]
+            channel = parts[3]
+            battery = parts[4]
+            humidity = parts[9]
+            deviceid = parts[21]
+            temperature = parts[22]
+
+            syslog.syslog('Channel: ' + channel + " Temp: " + str(temperature) + " Humidity: " + str(humidity) + " Battery: " + battery)
+            # TODO: syslog.syslog(debugMessage)
+            lastReadTime = arrow.utcnow()
+            nowFormatted = time.strftime('%Y-%m-%d %H:%M:%S')
+
+            for whitespace_character in string.whitespace:
+                channel = channel.replace(whitespace_character,'')
+
+            for whitespace_character in string.whitespace:
+                temperature = temperature.replace(whitespace_character,'')
+
+            for whitespace_character in string.whitespace:
+                humidity = humidity.replace(whitespace_character,'')
+
+                #p rint response
+
+            # Publish to mqtt
+            msgs = [
+                {'topic':"home/humidity" + channel, 'payload':str(humidity)},
+                {'topic':"home/temperature" + channel, 'payload':str(temperature)},
+                {'topic':"home/lastupdateraw" + channel, 'payload':str(datetime.datetime.now())},
+                {'topic':"home/battery" + channel, 'payload':battery}]
+            publish.multiple(msgs, hostname="amelia")
+
+            if (len(channel) > 0 and len(temperature) > 0 and len(humidity) > 0 and int(humidity) > 20 and float(temperature) > 60.0 and float(temperature) < 100.0):
+
+                # Wait 10 seconds then retry
+                time.sleep(10)
+
+        except Exception as ex:
             _, _, ex_traceback = sys.exc_info()
             log_traceback(ex, ex_traceback)
-        except:
-            syslog.syslog("Unhandled exception trying to collect exception information -sigh")
-
-        #time.sleep(30)
-
-                
-
+            time.sleep(1)
+            continue
